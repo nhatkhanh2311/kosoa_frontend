eedc6fec463a8a6e21ca1c5cd2ffe804c7927534
Nikkei <nhatkhanh2311@gmail.com>
2022-02-07 21:26:34 +0700

    khanh 20220207 2127: fix sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,9 +3,9 @@ import axios from "../stores/axios";
 import signContext from "../stores/sign-context";
 import snackbarContext from "../stores/snackbar-context";
 import {
-  Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, FormControlLabel, Grid, Radio,
-  RadioGroup, TextField, Typography
+  Box, Button, Dialog, DialogActions, DialogContent, DialogTitle, FormControl, Grid, TextField, ToggleButton, Typography
 } from "@mui/material";
+import {MenuBook as MenuBookIcon, School as SchoolIcon} from "@mui/icons-material";
 import {DatePicker, LocalizationProvider} from "@mui/lab";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import {vi} from "date-fns/locale";
@@ -85,8 +85,7 @@ function SignUp() {
     return ok;
   }
 
-  const onSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = () => {
     if (validate()) {
       setDisableButton(true);
       axios
@@ -126,103 +125,108 @@ function SignUp() {
       </DialogTitle>
 
       <DialogContent>
-        <form onSubmit={onSubmit}>
-          <FormControl fullWidth>
-            <Grid container spacing={1}>
-              <Grid item md={2}/>
+        <FormControl fullWidth>
+          <Grid container spacing={1}>
+            <Grid item md={2}/>
 
-              <Grid item md={8}>
-                <TextField id="username" label="Tên đăng nhập" type="text" variant="outlined"
-                           fullWidth sx={{mt: 0.7}} autoFocus
-                           error={validations.username || validations.usernameSpace || validations.usernameTaken}
-                           helperText={validations.username ? "Tên đăng nhập không được để trống!" :
-                             validations.usernameSpace ? "Tên đăng nhập không được có khoảng trắng!" :
-                               validations.usernameTaken ? "Tên đăng nhập đã được sử dụng!" : ""}
-                           onChange={(e) => setUsername(e.currentTarget.value)}/>
-              </Grid>
-
-              <Grid item md={6}>
-                <TextField id="password" label="Mật khẩu" type="password" variant="outlined"
-                           fullWidth sx={styles.input} error={validations.password || validations.passwordSpace}
-                           helperText={validations.password ? "Mật khẩu không ít hơn 8 ký tự!" :
-                             validations.passwordSpace ? "Mật khẩu không được có khoảng trắng!" : ""}
-                           onChange={(e) => setPassword(e.currentTarget.value)}/>
-              </Grid>
-
-              <Grid item md={6}>
-                <TextField id="repass" label="Nhập lại mật khẩu" type="password" variant="outlined"
-                           fullWidth sx={styles.input} error={validations.repass}
-                           helperText={validations.repass ? "Chưa khớp với mật khẩu!" : ""}
-                           onChange={(e) => setRepass(e.currentTarget.value)}/>
-              </Grid>
-
-              <Grid item md={6}>
-                <TextField id="name" label="Họ và tên" type="text" variant="outlined"
-                           fullWidth sx={styles.input} error={validations.name}
-                           helperText={validations.name ? "Tên không được để trống!" : ""}
-                           onChange={(e) => setName(e.currentTarget.value)}/>
-              </Grid>
-
-              <Grid item md={6}>
-                <TextField id="email" label="Địa chỉ email" type="email" variant="outlined"
-                           fullWidth sx={styles.input} error={validations.email || validations.emailTaken}
-                           helperText={validations.email ? "Email không được để trống!" :
-                             validations.emailTaken ? "Email đã được sử dụng!" : ""}
-                           onChange={(e) => setEmail(e.currentTarget.value)}/>
-              </Grid>
-
-              <Grid item md={6}>
-                <Box sx={styles.input}>
-                  <LocalizationProvider id="birthday" dateAdapter={AdapterDateFns} locale={vi}>
-                    <DatePicker label="Ngày sinh (không bắt buộc)" variant="standard" cancelText="Hủy"
-                                onChange={(newDate) => setBirthday(newDate)} value={birthday}
-                                renderInput={(params) => <TextField {...params}/>}/>
-                  </LocalizationProvider>
-                </Box>
-              </Grid>
-
-              <Grid item md={6}>
-                <TextField id="phone" label="Số điện thoại (không bắt buộc)" type="text" variant="outlined"
-                           fullWidth sx={styles.input} error={validations.phone}
-                           helperText={validations.phone ? "Số điện thoại chỉ bao gồm chữ số!" : ""}
-                           onChange={(e) => setPhone(e.currentTarget.value)}/>
-              </Grid>
+            <Grid item md={8}>
+              <TextField id="username" label="Tên đăng nhập" type="text" variant="outlined"
+                         fullWidth sx={{mt: 0.7}} autoFocus
+                         error={validations.username || validations.usernameSpace || validations.usernameTaken}
+                         helperText={validations.username ? "Tên đăng nhập không được để trống!" :
+                           validations.usernameSpace ? "Tên đăng nhập không được có khoảng trắng!" :
+                             validations.usernameTaken ? "Tên đăng nhập đã được sử dụng!" : ""}
+                         onChange={(e) => setUsername(e.currentTarget.value)}/>
             </Grid>
 
-            <Typography sx={styles.role}>
-              Chức năng tài khoản
-            </Typography>
-
-            <RadioGroup row value={role} onChange={(e) => setRole(e.currentTarget.value)}>
-              <Grid container spacing={2}>
-                <Grid item md={6} sx={styles.radio}>
-                  <FormControlLabel control={<Radio/>} label="Người học" value="student"/>
-                </Grid>
-                <Grid item md={6} sx={styles.radio}>
-                  <FormControlLabel control={<Radio/>} label="Người dạy" value="teacher"/>
-                </Grid>
-              </Grid>
-            </RadioGroup>
-
-            <Grid container spacing={2}>
-              <Grid item md={6} sx={styles.radio}>
-                <Typography>
-                  Bạn có thể học theo chương trình của web hoặc tham gia lớp học của người dạy.
-                </Typography>
-              </Grid>
-
-              <Grid item md={6} sx={styles.radio}>
-                <Typography>
-                  Bạn có thể tạo lớp học của riêng bạn.
-                </Typography>
-              </Grid>
+            <Grid item md={6}>
+              <TextField id="password" label="Mật khẩu" type="password" variant="outlined"
+                         fullWidth sx={styles.input} error={validations.password || validations.passwordSpace}
+                         helperText={validations.password ? "Mật khẩu không ít hơn 8 ký tự!" :
+                           validations.passwordSpace ? "Mật khẩu không được có khoảng trắng!" : ""}
+                         onChange={(e) => setPassword(e.currentTarget.value)}/>
             </Grid>
 
-            <Button disabled={disableButton} variant="contained" color="success" sx={styles.button} type="submit">
-              đăng ký
-            </Button>
-          </FormControl>
-        </form>
+            <Grid item md={6}>
+              <TextField id="repass" label="Nhập lại mật khẩu" type="password" variant="outlined"
+                         fullWidth sx={styles.input} error={validations.repass}
+                         helperText={validations.repass ? "Chưa khớp với mật khẩu!" : ""}
+                         onChange={(e) => setRepass(e.currentTarget.value)}/>
+            </Grid>
+
+            <Grid item md={6}>
+              <TextField id="name" label="Họ và tên" type="text" variant="outlined"
+                         fullWidth sx={styles.input} error={validations.name}
+                         helperText={validations.name ? "Tên không được để trống!" : ""}
+                         onChange={(e) => setName(e.currentTarget.value)}/>
+            </Grid>
+
+            <Grid item md={6}>
+              <TextField id="email" label="Địa chỉ email" type="email" variant="outlined"
+                         fullWidth sx={styles.input} error={validations.email || validations.emailTaken}
+                         helperText={validations.email ? "Email không được để trống!" :
+                           validations.emailTaken ? "Email đã được sử dụng!" : ""}
+                         onChange={(e) => setEmail(e.currentTarget.value)}/>
+            </Grid>
+
+            <Grid item md={6}>
+              <Box sx={styles.input}>
+                <LocalizationProvider id="birthday" dateAdapter={AdapterDateFns} locale={vi}>
+                  <DatePicker label="Ngày sinh (không bắt buộc)" variant="standard" cancelText="Hủy"
+                              onChange={(newDate) => setBirthday(newDate)} value={birthday}
+                              renderInput={(params) => <TextField {...params}/>}/>
+                </LocalizationProvider>
+              </Box>
+            </Grid>
+
+            <Grid item md={6}>
+              <TextField id="phone" label="Số điện thoại (không bắt buộc)" type="text" variant="outlined"
+                         fullWidth sx={styles.input} error={validations.phone}
+                         helperText={validations.phone ? "Số điện thoại chỉ bao gồm chữ số!" : ""}
+                         onChange={(e) => setPhone(e.currentTarget.value)}/>
+            </Grid>
+          </Grid>
+
+          <Typography textAlign="center" color="black" fontSize={20} mt={2} mb={1}>
+            Chức năng tài khoản
+          </Typography>
+
+          <Grid container spacing={2}>
+            <Grid item md={6} textAlign="center">
+              <ToggleButton value="student" selected={role === "student"} color="success" sx={styles.radio}
+                            onClick={() => setRole("student")}>
+                <MenuBookIcon sx={{mr: 1}}/>
+                Người học
+              </ToggleButton>
+            </Grid>
+
+            <Grid item md={6} textAlign="center">
+              <ToggleButton value="teacher" selected={role === "teacher"} color="success" sx={styles.radio}
+                            onClick={() => setRole("teacher")}>
+                <SchoolIcon sx={{mr: 1}}/>
+                Người dạy
+              </ToggleButton>
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={2}>
+            <Grid item md={6}>
+              <Typography textAlign="center">
+                Học theo chương trình của web hoặc tham gia lớp học của người dạy.
+              </Typography>
+            </Grid>
+
+            <Grid item md={6}>
+              <Typography textAlign="center">
+                Tạo lớp học của riêng bạn.
+              </Typography>
+            </Grid>
+          </Grid>
+
+          <Button disabled={disableButton} variant="contained" color="success" sx={styles.button} onClick={onSubmit}>
+            đăng ký
+          </Button>
+        </FormControl>
       </DialogContent>
 
       <DialogActions>
@@ -236,27 +240,22 @@ export default SignUp;
 
 const styles = {
   title: {
-    fontSize: 30,
-    textAlign: "center",
     mb: 0.5,
+    textAlign: "center",
+    fontSize: 30,
     fontWeight: "bold"
   },
   input: {
     my: 0
   },
   button: {
+    mx: 5,
     mt: 3,
     height: 50,
-    mx: 5,
     fontSize: 18
   },
-  role: {
-    mt: 2,
-    textAlign: "center",
-    color: "black",
-    fontSize: 20
-  },
   radio: {
-    textAlign: "center"
+    textTransform: "none",
+    height: 40
   }
 }
